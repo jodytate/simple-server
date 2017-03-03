3ad8159228798d9fb96fe685837662e0890d0079
jody <jtate@uw.edu>
2017-03-03 15:23:15 -0800

    use const

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
+const app = express();
 
-var www = express.static(__dirname + '/www');
+const www = express.static(__dirname + '/www');
 
 app.use('/', www);
 
